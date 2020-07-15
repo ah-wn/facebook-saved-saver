c6a3fd50761d37923b09b841b212e1ef32a8aba0
Axmin <ahwn@pm.me>
2020-07-15 14:45:55 +0545

    Fixed original url parse logic

--- fb/entities/saved_item.py
+++ fb/entities/saved_item.py
@@ -24,12 +24,11 @@ class SavedItem(object):
                 span = info.find('div._3vo5._268b', first=1).text
                 self.photos = parser.post_item_if_photos(span)
 
-        try:
-            self.orig = html._make_absolute(
-                html.find('a._24-t[href]', first=1).attrs['href'])
-        except AttributeError as e:
-            print(e, self.url, span, '=================')
-            self.orig = self.url
+        orig = html.find('a._24-t[href]', first=1)
+        self.orig = None
+        if orig:
+            self.orig = html._make_absolute(orig
+                                            .attrs['href'])
 
     def __repr__(self):
         return pformat(vars(self))
