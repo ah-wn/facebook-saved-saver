b8b3cb585a621f238d88bf38b2c51791354de4b3
Axmin <ahwn@pm.me>
2020-07-15 11:21:12 +0545

    Create Facebook object

---
+++ fb/__init__.py
@@ -0,0 +1,62 @@
+from requests_html import (HTMLSession, HTML)
+from youtube_dl import YoutubeDL
+from pathlib import Path
+from . import parser
+import shelve
+
+
+class Facebook(object):
+    parse = parser
+    URL = 'https://facebook.com'
+
+    class UserNotLoggedIn(Exception):
+        pass
+
+    def graph_url(self, id_):
+        return f'http://graph.facebook.com/{id_}/picture'
+
+    path = Path(__file__).parent / '.data'
+    path.mkdir(exist_ok=True)
+
+    def config(self):
+        return shelve.open(f"{self.path / '.config'}", 'c')
+
+    def __init__(self):
+        self.client = HTMLSession()
+        self.__csrf = self.config()
+
+    @property
+    def csrf(self):
+        return self.__csrf['csrf']
+
+    @csrf.setter
+    def csrf(self, data):
+        self.__csrf['csrf'] = data
+
+    @classmethod
+    def join(cls, url):
+        return cls.parse.parse.urljoin(cls.URL, url)
+
+    def is_logged_in(self):
+        for i in self.cookies:
+            if i.domain == '.facebook.com' and i.name == 'c_user':
+                return int(i.value)
+        else:
+            return False
+
+    @property
+    def cookies(self):
+        return self.client.cookies
+
+    @cookies.setter
+    def cookies(self, value):
+        self.client.cookies = value
+
+    def home(self):
+        return self.client.get(self.URL)
+
+    def youtube_dl(self, *args, **kwargs):
+        ydl = YoutubeDL(**kwargs)
+        for cookie in self.cookies:
+            ydl.cookiejar.set_cookie(cookie)
+        return ydl
